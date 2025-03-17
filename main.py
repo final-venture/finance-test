7bef3fed98622c39068df377d757a00ce1de289a
Cyril Chen <2385658016cc@gmail.com>
2025-03-18 11:01:20 +1300

    .

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import pdfplumber
+from pdf2image import convert_from_path
+import pytesseract
+import pandas as pd
+
+def ocr_pdf_to_text(pdf_path):
+    images = convert_from_path(pdf_path, dpi=400)
+    all_text = []
+    for img in images:
+        text = pytesseract.image_to_string(img)
+        all_text.append(text)
+    return "\n".join(all_text)
 
 pdf_path = "examples/cvt24.pdf"
-with pdfplumber.open(pdf_path) as pdf:
-    for page in pdf.pages:
-        tables = page.extract_tables()
-        for table in tables:
-            print(table)
+raw_text = ocr_pdf_to_text(pdf_path)
+print(raw_text)
+
+with open('output.txt', 'w') as file:
+    file.write(raw_text)
